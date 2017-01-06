523fb807ec6d8059dc90ceb1a3b53292a4f33969
Megha Gupta <gupta34@email.sc.edu>
2017-01-06 15:05:11 -0800

    *Add a input functional based component in search bar *export SearchBar component and import in index file *Add youtube api key

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react' ;
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar'
 
 
 //New componnet that will produce html 
-
+const API_KEY = 'AIzaSyBFHtMVjjZPYoGINMOtJWp16iAPerHYn80';
 
 const App = () => {
-	return <div> Hello React JS! </div>;
+	return (
+	<div> 
+	<SearchBar />
+	</div>
+	);
 }
 
 // take this component's gernerated html and put it on page (in dom )
 
-ReactDOM.render(<App/>, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
