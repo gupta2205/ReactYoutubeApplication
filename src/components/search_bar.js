ce489e7290ec8ea2739563538270cb3cec5ce891
Megha Gupta <gupta34@email.sc.edu>
2017-01-06 16:05:37 -0800

    Create state obj in CBC in it's constructor event handelling on input and use of => in catching event

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,15 +2,34 @@ import React,{ Component } from 'react';
 
 //class based component(CBC) need a render function and JSX in it 
 class SearchBar extends Component {
+
+//stete is single JS obj, 
+//Every CBC have it's own state, if state of that cbc chnage it will rerender with it's children
+//defining state in constrsctor 
+	constructor(props){
+		super(props);
+		this.state = {terms: ''};
+	}
+
 	render(){
-		return <input />; 
+		return (
+			<div>
+			<input onChange={this.onInputChange} />
+			<input onChange={event => console.log(event.target.value)} />
+			</div>
+		); 
+	}
+
+	onInputChange(event) {
+		console.log(event);
+
 	}
 }
 
 
-//functional comp.
+// functional based comp.
 // const SearchBar = () => {
-// 	return <input />; 
+// 	return <input /> 
 // };
 
 
